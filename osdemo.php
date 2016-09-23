8bcc8c30c673c4d8f1969dd8679c91dac7f3e8ff
Mickael Di-Monte <mickael.di-monte_ext@michelin.com>
2016-09-23 18:03:59 +0200

    use mysqli instead of mysql

--- osdemo.php
+++ osdemo.php
@@ -1,14 +1,12 @@
 <?php
 
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
-
 $dbhost = getenv("DB_SERVICE_HOST");
 $dbport = getenv("DB_SERVICE_PORT");
 $dbname = getenv("MYSQL_DATABASE");
 $dbuser = getenv("MYSQL_USER");
 $dbpassword = getenv("MYSQL_PASSWORD");
 
-$connection = mysql_connect($dbhost . ":" . $dbport, $dbuser, $dbpassword);
+$connection = mysqli_connect($dbhost . ":" . $dbport, $dbuser, $dbpassword, $dbname);
 
 if (!$connection) {
 	echo "Failed to connect to the database using these info:<br>";
@@ -18,16 +16,15 @@ if (!$connection) {
 	echo "dbuser = " . $dbuser . "<br>";
 	echo "dbpassword = " . $dbpassword . "<br>";
 } else {
-	echo "Successfully connected to the database '" . $dbname . "'.<br><br>";
+	echo "Successfully connected to the database '" . $dbname . "'.<br>";
+	echo "Funny, how things can sometimes go as planned :)<br><br>";
 
-	$dbconnection = mysql_select_db($dbname);
-
-	$rs = mysql_query("SELECT * FROM users");
-	while ($row = mysql_fetch_assoc($rs)) {
+	$rs = $connection->query("SELECT * FROM users");
+	while ($row = mysqli_fetch_assoc($rs)) {
 		echo "id.: " . $row['user_id'] . " / name: " . $row['username'] . "<br>";
 	}
 
-	mysql_close();
+	mysqli_close();
 }
 
 ?>
