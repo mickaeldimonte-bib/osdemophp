57dd93d8628429829d1e0cde84aa1290c7ed58ee
Mickael Di-Monte <mickael.di-monte_ext@michelin.com>
2016-09-23 17:12:43 +0200

    hide warnings

--- osdemo.php
+++ osdemo.php
@@ -1,4 +1,7 @@
 <?php
+
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
+
 $dbhost = getenv("DB_SERVICE_HOST");
 $dbport = getenv("DB_SERVICE_PORT");
 $dbname = getenv("MYSQL_DATABASE");
@@ -8,23 +11,23 @@ $dbpassword = getenv("MYSQL_PASSWORD");
 $connection = mysql_connect($dbhost . ":" . $dbport, $dbuser, $dbpassword);
 
 if (!$connection) {
-	echo "Failed to connect to the database";
-	echo "dbhost = ".$dbhost;
-	echo "dbport = ".$dbport;
-	echo "dbname = ".$dbname;
-	echo "dbuser = ".$dbuser;
-	echo "dbpassword = ".$dbpassword;
+	echo "Failed to connect to the database using these info:<br>";
+	echo "dbhost = " . $dbhost . "<br>";
+	echo "dbport = " . $dbport . "<br>";
+	echo "dbname = " . $dbname . "<br>";
+	echo "dbuser = " . $dbuser . "<br>";
+	echo "dbpassword = " . $dbpassword . "<br>";
 } else {
-	echo "Successfully connected to the database '" . $dbname . "'.<br>";
+	echo "Successfully connected to the database '" . $dbname . "'.<br><br>";
+
+	$dbconnection = mysql_select_db($dbname);
+
+	$rs = mysql_query("SELECT * FROM users");
+	while ($row = mysql_fetch_assoc($rs)) {
+		echo "id.: " . $row['user_id'] . " / name: " . $row['username'] . "<br>";
+	}
+
+	mysql_close();
 }
-
-$dbconnection = mysql_select_db($dbname);
-
-$rs = mysql_query("SELECT * FROM users");
-while ($row = mysql_fetch_assoc($rs)) {
-	echo "id.: " . $row['user_id'] . " / name: " . $row['username'] . "<br>";
-}
-
-mysql_close();
 
 ?>
