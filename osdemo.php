666da9f2e3c569193a8771a0f115e78cedf638d3
Mickael Di-Monte <mickael.di-monte_ext@michelin.com>
2016-09-26 14:30:45 +0200

    enhance display

--- osdemo.php
+++ osdemo.php
@@ -6,9 +6,9 @@ $dbname = getenv("DATABASE_NAME");
 $dbuser = getenv("DATABASE_USER");
 $dbpassword = getenv("DATABASE_PASSWORD");
 
-//$host= gethostname();
-//$ip = gethostbyname($host);
-//echo "Just hit container '" . $host . "' (" . $ip . ")<br><br>";
+$host= gethostname();
+$ip = gethostbyname($host);
+echo "Just hit container '" . $host . "' (" . $ip . ")<br><br>";
 
 $connection = mysqli_connect($dbhost . ":" . $dbport, $dbuser, $dbpassword, $dbname);
 
@@ -20,12 +20,12 @@ if (!$connection) {
 	echo "dbuser = " . $dbuser . "<br>";
 	echo "dbpassword = " . $dbpassword . "<br>";
 } else {
-	//echo "Successfully connected to the database '" . $dbname . "'.<br>";
-	//echo "Funny, how things can sometimes go as planned :)<br><br>";
+	echo "Successfully connected to the database '" . $dbname . "'.<br>";
+	echo "Funny, how things can sometimes go as planned :)<br><br>";
 
 	$rs = $connection->query("SELECT * FROM users");
 	while ($row = mysqli_fetch_assoc($rs)) {
-		echo "id.: " . $row['user_id'] . " name: " . $row['username'] . "<br>";
+		echo "id.: " . $row['user_id'] . " / name: " . $row['username'] . "<br>";
 	}
 
 	mysqli_close($connection);
